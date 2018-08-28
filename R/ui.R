5a2de8b5c93876e1b76a6a65f57ebd105c2c9b6a
Unknown <lars.gerdes95@gmail.com>
2018-08-28 21:27:04 +0200

    added shiny:: in ui

--- R/ui.R
+++ R/ui.R
@@ -1,18 +1,18 @@
 #### UI ####
-ui <- fluidPage(
-  titlePanel(
+ui <- shiny::fluidPage(
+  shiny::titlePanel(
     title = "Fit Distributions to Your Data",
     windowTitle = "Shiny.Distributions"
   ),
-  tabsetPanel(
+  shiny::tabsetPanel(
     #### Tab: Plot Distribution ####
-    tabPanel(
+    shiny::tabPanel(
       title = "Plot Distribution",
       # Sidebar layout with input and output definitions
-      sidebarLayout(
-        sidebarPanel = sidebarPanel(
+      shiny::sidebarLayout(
+        sidebarPanel = shiny::sidebarPanel(
           # Output: Select variable
-          uiOutput(outputId = "variable"),
+          shiny::uiOutput(outputId = "variable"),
           # Input: Select plot type
           # selectInput(
           #   inputId = "plot_type",
@@ -20,11 +20,11 @@ ui <- fluidPage(
           #   choices = list("Histogram", "Empiric Density Curve")
           #),
           # Output: Select plot type
-          uiOutput(outputId = "plot_type"),
+          shiny::uiOutput(outputId = "plot_type"),
           # Output: Bins for Histogram
-          uiOutput(outputId = "num_bins"),
+          shiny::uiOutput(outputId = "num_bins"),
           # Function type
-          radioButtons(
+          shiny::radioButtons(
             inputId = "fun_type",
             label = "Function Type",
             choices = c("all", "continuous", "discrete", "mixed"),
@@ -32,7 +32,7 @@ ui <- fluidPage(
             inline = TRUE
           ),
           # Number of parameters
-          radioButtons(
+          shiny::radioButtons(
             inputId = "parameters",
             label = "Number of Parameters",
             choices = c("all", 1, 2, 3, 4),
@@ -40,77 +40,78 @@ ui <- fluidPage(
             inline = TRUE
           ),
           # Input: Select density function
-          selectInput(
+          shiny::selectInput(
             inputId = "dist",
             label = "Density Function",
             choices = distributions$dist_density
           ),
-          uiOutput(outputId = "num_location"),
-          uiOutput(outputId = "num_scale"),
-          uiOutput(outputId = "num_skewness"),
-          uiOutput(outputId = "num_kurtosis"),
-          uiOutput(outputId = "num_binomial_denominators"),
+          shiny::uiOutput(outputId = "num_location"),
+          shiny::uiOutput(outputId = "num_scale"),
+          shiny::uiOutput(outputId = "num_skewness"),
+          shiny::uiOutput(outputId = "num_kurtosis"),
+          shiny::uiOutput(outputId = "num_binomial_denominators"),
           # Horizontal line
-          tags$hr(),
-          uiOutput(outputId = "num_max_y"),
-          uiOutput(outputId = "num_max_x"),
-          uiOutput(outputId = "num_min_x")
+          shiny::tags$hr(),
+          shiny::uiOutput(outputId = "num_max_y"),
+          shiny::uiOutput(outputId = "num_max_x"),
+          shiny::uiOutput(outputId = "num_min_x")
         ),
-        mainPanel = mainPanel(
-          h3(textOutput(outputId = "caption")),
-          uiOutput(outputId = "plot"),
-          br(),
-          br(),
-          h4("Default Parameter(s) of Theoretic Density Function:"),
-          tags$b(textOutput(outputId = "info_text"), style = "font-size:120%"),
-          br(),
-          h4(textOutput(outputId = "head_ml_estimates")),
-          tags$b(textOutput(outputId = "info_text2"),
+        mainPanel = shiny::mainPanel(
+          shiny::h3(shiny::textOutput(outputId = "caption")),
+          shiny::uiOutput(outputId = "plot"),
+          shiny::br(),
+          shiny::br(),
+          shiny::h4("Default Parameter(s) of Theoretic Density Function:"),
+          shiny::tags$b(shiny::textOutput(outputId = "info_text"),
+                        style = "font-size:120%"),
+          shiny::br(),
+          shiny::h4(shiny::textOutput(outputId = "head_ml_estimates")),
+          shiny::tags$b(shiny::textOutput(outputId = "info_text2"),
                  style = "font-size:120%")
         )
       )
     ),
     #### Tab: Data upload ####
-    tabPanel(
+    shiny::tabPanel(
       title = "Data Upload",
       # Sidebar layout with input and output definitions
-      sidebarLayout(
-        sidebarPanel = sidebarPanel(
+      shiny::sidebarLayout(
+        sidebarPanel = shiny::sidebarPanel(
           width = 2,
           # Input: Select a file
-          fileInput(
+          shiny::fileInput(
             inputId = "dataset",
             label = "Choose CSV-file",
             multiple = TRUE,
             accept = c("text/csv",
                        "text/comma-separated-values,text/plain", ".csv")
           ),
-          actionButton('reset', 'Reset Input'),
+          shiny::actionButton('reset', 'Reset Input'),
           #uiOutput(outputId = "stop_mle"),
           # Horizontal line
-          tags$hr(),
+          shiny::tags$hr(),
           # Input: Checkbox if file has header
-          checkboxInput(
+          shiny::checkboxInput(
             inputId = "header",
             label = "Header",
             value = TRUE
           ),
           # Input: Select separator
-          radioButtons(
+          shiny::radioButtons(
             inputId = "sep",
             label = "Seperator",
             choices = c(Comma = ",", Semicolon = ";", Tab = "\t"),
             selected = ","
           ),
           # Input: Select quotes
-          radioButtons(
+          shiny::radioButtons(
             inputId = "quote",
             label = "Quote",
             choices = c(None = "", "Double Quote" = '"', "Single Quote" = "'"),
             selected = '"'
           ),
           # Horizontal line
-          tags$hr(),
+          shiny::tags$hr(),
           # Input: Select number of rows to display
           #radioButtons(
           #  inputId = "display",
@@ -119,30 +120,31 @@ ui <- fluidPage(
           #  selected = "head"
           #),
           #Input: Select data table or summary of data set
-          radioButtons(
+          shiny::radioButtons(
             inputId = "display",
             label = "Display",
             choices = c("Data Frame", "Summary"),
             selected = "Data Frame"
           ),
-          tags$hr(),
+          shiny::tags$hr(),
           # R-Datasets
-          selectInput(
+          shiny::selectInput(
             inputId = "r_dataset",
             label = "Choose a R-dataset:",
             choices = c("None", "iris", "diamonds", "mtcars")
           )
         ),
-        mainPanel = mainPanel(
+        mainPanel = shiny::mainPanel(
           DT::dataTableOutput(outputId = "contents"),
           #tableOutput(outputId = "contents"),
-          verbatimTextOutput(outputId = "summary")
+          shiny::verbatimTextOutput(outputId = "summary")
         )
       )
     ),
     #### Tab: Properties of Distributions ####
-    tabPanel(
+    shiny::tabPanel(
       title = "Properties of Distributions",
       DT::dataTableOutput(outputId = "distributions_tab")
     )
-  ))
+  )
+)

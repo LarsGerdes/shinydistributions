338af4c50fa331d87b34f0aca0040a001e2ec611
Unknown <lars.gerdes95@gmail.com>
2018-11-27 21:05:47 +0100

    improving bounds

--- data-raw/data_frames.R
+++ data-raw/data_frames.R
@@ -4,17 +4,32 @@ distributions <- read.csv(file = "data-raw/distributions_ranges.csv",
                           header = TRUE, sep = ';', stringsAsFactors = FALSE)
 # omit conditions for ranges
 distributions <- distributions[, - c(12:22)]
+# improving bounds
+distributions[which(distributions$scale_lower_bound == 1e-10),
+              "scale_lower_bound"] <- 1e-6
+distributions[which(distributions$scale_lower_bound == 0),
+              "scale_lower_bound"] <- 1e-2
+distributions[distributions$dist_density == "Logit Normal - dLOGITNO",
+              "x_upper_bound"] <- 1
 
 
+
+
+# read csv containing relevant columns for tab "Properties of Distributions"
+distributions_tab <- read.csv(file = "data-raw/distributions_tab_ranges.csv",
+                              header = TRUE, sep = ';',
+                              stringsAsFactors = FALSE, check.names = FALSE)
+# replace NA's by "-" for visual purposes
+distributions_tab[which(is.na(distributions_tab), arr.ind = TRUE)] <- "-"
+
+# save as sysdata.rda
+usethis::use_data(distributions, distributions_tab, internal = TRUE,
+                  overwrite = TRUE)
 
 # distributions[which(distributions$location_lower_bound == 1e-10),
 #               "location_lower_bound"] <- 1e-6
 # distributions[which(distributions$location_lower_bound == 0),
 #               "location_lower_bound"] <- 1e-2
-distributions[which(distributions$scale_lower_bound == 1e-10),
-              "scale_lower_bound"] <- 1e-6
-distributions[which(distributions$scale_lower_bound == 0),
-              "scale_lower_bound"] <- 1e-2
 # distributions[which(distributions$skewness_lower_bound == 1e-10),
 #               "skewness_lower_bound"] <- 1e-6
 # distributions[which(distributions$skewness_lower_bound == 0),
@@ -31,14 +46,3 @@ distributions[which(distributions$scale_lower_bound == 0),
 #               "skewness_upper_bound"] <- 0.999999
 # distributions[which(distributions$kurtosis_upper_bound == 0.99999999),
 #               "kurtosis_upper_bound"] <- 0.999999
-
-# read csv containing relevant columns for tab "Properties of Distributions"
-distributions_tab <- read.csv(file = "data-raw/distributions_tab_ranges.csv",
-                              header = TRUE, sep = ';',
-                              stringsAsFactors = FALSE, check.names = FALSE)
-# replace NA's by "-" for visual purposes
-distributions_tab[which(is.na(distributions_tab), arr.ind = TRUE)] <- "-"
-
-# save as sysdata.rda
-usethis::use_data(distributions, distributions_tab, internal = TRUE,
-                  overwrite = TRUE)

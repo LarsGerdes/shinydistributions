54a7409e4b9fc833ed5d532c3008cde80e49fa68
Unknown <lars.gerdes95@gmail.com>
2018-11-27 17:24:12 +0100

    improved bounds

--- data-raw/data_frames.R
+++ data-raw/data_frames.R
@@ -7,22 +7,30 @@ distributions <- distributions[, - c(12:22)]
 
 
 
-distributions[which(distributions$location_lower_bound == 1e-10),
-              "location_lower_bound"] <- 1e-6
+# distributions[which(distributions$location_lower_bound == 1e-10),
+#               "location_lower_bound"] <- 1e-6
+# distributions[which(distributions$location_lower_bound == 0),
+#               "location_lower_bound"] <- 1e-2
 distributions[which(distributions$scale_lower_bound == 1e-10),
               "scale_lower_bound"] <- 1e-6
-distributions[which(distributions$skewness_lower_bound == 1e-10),
-              "skewness_lower_bound"] <- 1e-6
-distributions[which(distributions$kurtosis_lower_bound == 1e-10),
-              "kurtosis_lower_bound"] <- 1e-6
-distributions[which(distributions$location_upper_bound == 0.99999999),
-              "location_upper_bound"] <- 0.999999
-distributions[which(distributions$scale_upper_bound == 0.99999999),
-              "scale_upper_bound"] <- 0.999999
-distributions[which(distributions$skewness_upper_bound == 0.99999999),
-              "skewness_upper_bound"] <- 0.999999
-distributions[which(distributions$kurtosis_upper_bound == 0.99999999),
-              "kurtosis_upper_bound"] <- 0.999999
+distributions[which(distributions$scale_lower_bound == 0),
+              "scale_lower_bound"] <- 1e-2
+# distributions[which(distributions$skewness_lower_bound == 1e-10),
+#               "skewness_lower_bound"] <- 1e-6
+# distributions[which(distributions$skewness_lower_bound == 0),
+#               "skewness_lower_bound"] <- 1e-2
+# distributions[which(distributions$kurtosis_lower_bound == 1e-10),
+#               "kurtosis_lower_bound"] <- 1e-6
+# distributions[which(distributions$kurtosis_lower_bound == 0),
+#               "kurtosis_lower_bound"] <- 1e-2
+# distributions[which(distributions$location_upper_bound == 0.99999999),
+#               "location_upper_bound"] <- 0.999999
+# distributions[which(distributions$scale_upper_bound == 0.99999999),
+#               "scale_upper_bound"] <- 0.999999
+# distributions[which(distributions$skewness_upper_bound == 0.99999999),
+#               "skewness_upper_bound"] <- 0.999999
+# distributions[which(distributions$kurtosis_upper_bound == 0.99999999),
+#               "kurtosis_upper_bound"] <- 0.999999
 
 # read csv containing relevant columns for tab "Properties of Distributions"
 distributions_tab <- read.csv(file = "data-raw/distributions_tab_ranges.csv",
@@ -32,5 +40,5 @@ distributions_tab <- read.csv(file = "data-raw/distributions_tab_ranges.csv",
 distributions_tab[which(is.na(distributions_tab), arr.ind = TRUE)] <- "-"
 
 # save as sysdata.rda
-devtools::use_data(distributions, distributions_tab, internal = TRUE,
-                   overwrite = TRUE)
+usethis::use_data(distributions, distributions_tab, internal = TRUE,
+                  overwrite = TRUE)
